ac19926305a1226e0ddcbd7b2ef64d43e6f3bbde
KateGir <40317363+KateGir@users.noreply.github.com>
2018-09-22 22:21:42 +0300

    add label label changed position

--- MedicalApp/PatientCardWindow.xaml.cs
+++ MedicalApp/PatientCardWindow.xaml.cs
@@ -24,11 +24,6 @@ namespace MedicalApp
             InitializeComponent();
         }
 
-        private void txbAdress_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
 		private void btnDocSearch_Click(object sender, RoutedEventArgs e)
 		{
 
